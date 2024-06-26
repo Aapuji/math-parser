06778b7f00b56d45bd8009d26c95775359440201
Aapuji <om.robotics16@gmail.com>
2024-06-26 09:42:47 -0400

    Worked on it a bit more

--- lexer.c
+++ lexer.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "lexer.h"
 
@@ -11,23 +12,70 @@ Lexer init_lexer(const char *src) {
     };
 }
 
+void advance(Lexer *lexer) {
+    lexer->current++;
+}
+
 static bool is_at_end(Lexer *lexer) {
     return *lexer->current == '\0';
 }
 
-static is_whitespace(char c) {}
-
-static is_alpha(char c) {}
-
-static is_numeric(char c) {}
-
-static is_operator(char c) {}
-
-static void skip_whitespace(Lexer *lexer) {
-
+static bool is_whitespace(char c) {
+    return isspace((int) c);
 }
 
-static Token lex_ident(Lexer *lexer) {}
+static bool is_alpha(char c) {
+    return isalpha((int) c);
+}
+
+static bool is_numeric(char c) {
+    return c >= '0' && c <= '9';
+}
+
+static bool is_ident(char c) {
+    return is_alpha(c)
+        || is_numeric(c)
+        || c == '_';
+}
+
+static is_operator(char c) {
+    switch (c) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '^':
+            return true;
+        default:
+            return false;
+    };
+}
+
+static void skip_whitespace(Lexer *lexer) {
+    while (is_whitespace(*lexer->current)) {
+        if (*lexer->current == '\n')
+            lexer->line++;
+        
+        advance(lexer);
+    }
+}
+
+static Token lex_ident(Lexer *lexer) {
+    char *lexeme_start = lexer->current;
+
+    int i = 1;
+    advance(lexer);
+    for (; is_ident(*lexer->current); i++, advance(&lexer));
+
+    char *lexeme_end = lexer->current[-1];
+
+    return (Token) {
+        .type = TT_IDENT,
+        .start = lexeme_start,
+        .len = i,
+        .line = lexer->line
+    };
+}
 
 static Token lex_number(Lexer *lexer) {}
 
@@ -36,18 +84,18 @@ static Token lex_operator(Lexer *lexer) {}
 static void report_error(Lexer *lexer) {}
 
 static Token lex_token(Lexer *lexer) {
-    char c = lexer->current;
+    char c = *lexer->current;
 
-    if (is_whitespace(c)) {
-        skip_whitespace(&lexer);
-    } else if (is_alpha(c)) {
-        lex_ident(&lexer);
+    skip_whitespace(lexer);
+    
+    if (is_alpha(c)) {
+        lex_ident(lexer);
     } else if (is_numeric(c)) {
-        lex_number(c);
+        lex_number(lexer);
     } else if (is_operator(c)) {
-        lex_operator(c);
+        lex_operator(lexer);
     } else {
-        report_error(&lexer);
+        report_error(lexer);
     }
 }
 
